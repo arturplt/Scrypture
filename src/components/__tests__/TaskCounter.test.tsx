d2e2516ac6e320151c4ce54f9303b77d23058000
arturplt <50488590+arturplt@users.noreply.github.com>
2025-07-19 23:01:48 +0100

    Dependancies
    Adding missing dependencies

--- src/components/__tests__/TaskCounter.test.tsx
+++ src/components/__tests__/TaskCounter.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { TaskCounter } from '../TaskCounter';
 import { TaskProvider } from '../../hooks/useTasks';
